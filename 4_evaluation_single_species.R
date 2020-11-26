a3effab7001360c77b295b11de59f7d2fec01ed1
ChrKoenig <chr.koenig@outlook.com>
2020-11-26 12:09:19 +0100

    Improved evaluation & analysis

--- 4_evaluation_single_species.R
+++ 4_evaluation_single_species.R
@@ -7,46 +7,71 @@ load("Data/species_final.RData")
 
 # ------------------------------------------------------------------------------------- #
 #### Convergence ####
-load(sample(list.files("Data/models_fit/SSOM/", full.names = T), 1)) # sample model for random species
-jagsUI::traceplot(jags_samples)
+load("Data/models_fit/SSOM/run_nov_24/31227_MCMC.RData") 
+
+# look at p and psi of random model
+load(sample(list.files("Data/models_fit/SSOM/run_nov_24", full.names = T), 1)) 
+jagsUI::traceplot(jags_samples, parameters = c("alpha_null", "beta_null"))
+
+# create convergence summary table
+convergence_table = bind_rows(lapply(species_final$spec_id, FUN = function(spec){
+  if(!any(grepl(spec, list.files("Data/models_fit/SSOM/run_nov_24/")))){return(NULL)}
+  load(list.files("Data/models_fit/SSOM/run_nov_24/", pattern = as.character(spec), full.names = T))
+  as.data.frame(t(sapply(jags_samples$Rhat, function(x) all(x < 1.1)))) %>% 
+    add_column(spec_id = spec, .before = "alpha_null")
+}))
 
 #### Response curves ####
-plot_response = function(spec_id, variable, process){
+plot_response = function(spec, variable, process){
   # Prepare model results
-  load(paste0("Data/models_fit/SSOM/", spec_id, "_MCMC.RData"))
-  post_means = data.frame(summary(jags_samples)$statistics)
-  post_means$var_name = c("elevation", "elevation_sq", "day", "day_sq", "ddeg0","bio_12","rad","asp","slp","ddeg0_sq","bio_12_sq","rad_sq","asp_sq","slp_sq","mu_p","mu_psi")
-  coefs = post_means %>% 
-    rownames_to_column(var = "row_name") %>% 
-    mutate(process = case_when(grepl("alpha", row_name) ~ "detection",
-                               grepl("beta", row_name) ~ "state",
-                               TRUE ~ "probability")) %>% 
-    dplyr::filter(process == !!process | process == "probability")
+  load(paste0("Data/models_fit/SSOM/run_nov_24/", spec, "_MCMC.RData"))
+  if(process == "detection"){
+    intercept = jags_samples$sims.list$alpha_null 
+    coefs = jags_samples$sims.list$alpha_coef
+    colnames(coefs) = c("elev", "elev_sq", "day", "day_sq")
+  } else if (process == "state"){
+    intercept = jags_samples$sims.list$beta_null
+    coefs = jags_samples$sims.list$beta_coef
+    colnames(coefs) = c("ddeg0","bio_12","rad","asp","slp","ddeg0_sq","bio_12_sq","rad_sq","asp_sq","slp_sq")
+  } else {
+    stop("unknown process")
+  }
   
-  # Get coefficients
-  if(process == "state"){b0 = coefs$Mean[coefs$row_name == "mu_psi"]}
-  if(process == "detection"){b0 = coefs$Mean[coefs$row_name == "mu_p"]}
-  b1 = coefs$Mean[coefs$var_name == variable]
-  b2 = coefs$Mean[coefs$var_name == paste0(variable, "_sq")]
+  if(!variable %in% colnames(coefs)){
+    stop("unknown variable")
+  }
   
   # Rescale variable to original range
   load("Data/sample_sites.RData")
   if(variable != "day"){
-    var_orig = sample_sites@data[,variable]
+    var_orig = dplyr::select(sample_sites@data, contains(variable))[,1]
   } else {
     var_orig = 1:365
   }  
   var_lims = (range(var_orig) - mean(var_orig)) / sd(var_orig)
   
   # Plot
-  curve(plogis(b0 + b1*x + b2*x*x), var_lims[1], var_lims[2], col = "red", ylim = c(0, 1), axes = F, xlim = c(-2.5,2.5), main = spec,
-        xlab = variable, ylab = ifelse(process == "state", "psi (probability of occurrence)", "p (probability of detection)"))
-  axis(1, at = c(-2,-1,-0,1,2), labels = round(c(mean(var_orig)-2*sd(var_orig), mean(var_orig)-sd(var_orig), mean(var_orig), 
-                                                 mean(var_orig)+sd(var_orig), mean(var_orig)+2*sd(var_orig))))
-  axis(2, las = 1)
+  for(i in 1:nrow(coefs)){
+    if(i == 1){
+      curve(plogis(intercept[1] + coefs[1,variable]*x + coefs[1,paste0(variable, "_sq")]*x*x), 
+            from = var_lims[1], to = var_lims[2], col = "#FF000005", ylim = c(0, 1), axes = F, xlim = c(-3,3), main = spec,
+            xlab = variable, ylab = ifelse(process == "state", "psi (probability of occurrence)", "p (probability of detection)"))
+      axis(1, at = c(-3,-2,-1,-0,1,2,3), labels = round(c(mean(var_orig)-3*sd(var_orig), mean(var_orig)-2*sd(var_orig), mean(var_orig)-sd(var_orig), mean(var_orig), 
+           mean(var_orig)+sd(var_orig), mean(var_orig)+2*sd(var_orig), mean(var_orig)+3*sd(var_orig))))
+      axis(2, las = 1)
+      abline(v = var_lims[1])
+      abline(v = var_lims[2])
+    } else (
+      curve(plogis(intercept[i] + coefs[i,variable]*x + coefs[i,paste0(variable, "_sq")]*x*x),  from = var_lims[1], to = var_lims[2], add = T, col = "#FF000005")
+    )
+  }
+}
+for(spec in species_final$spec_id){
+  plot_response(spec, "elev", "detection")
+  Sys.sleep(0.1)
 }
 
 for(spec in species_final$spec_id){
-  plot_response(spec, "elevation", "detection")  
+  plot_response(spec, "slp", "state")
+  Sys.sleep(0.1)
 }
-
--- 6_analysis.R
+++ 6_analysis.R
@@ -1,5 +1,5 @@
 library("tidyverse")
-library("rjags")
+library("jagsUI")
 library("coda")
 
 setwd("~/ownCloud/Projects/Berlin/06 - Butterfly_detection/")
@@ -7,29 +7,30 @@ rm(list=ls())
 
 load("Data/species_final.RData")
 load("Data/traits_final.RData")
-load("Data/colors_extr.RData")
 
 # ------------------------------------------------------------------------------------- #
-#### organize model estimates ####
+#### organize SSOM model estimates ####
 antilogit = function(x){exp(x) / (1+exp(x))}
-model_estimates = bind_rows(lapply(species_final$spec_id, FUN = function(spec){
-  load(list.files("Data/model_fits/", pattern = as.character(spec), full.names = T))
-  tmp_summary = summary(jags_samples)$statistics
-  alpha_names = c("elev", "elev_sq", "day", "day_sq")
-  beta_names = c("ddeg0","bio_12","rad","asp","slp","ddeg0_sq","bio_12_sq","rad_sq","asp_sq","slp_sq")
-  prob_names = c("mean_p", "mean_psi")
-  
-  tmp_means = setNames(tmp_summary[,"Mean"], paste0(c(alpha_names, beta_names, prob_names), "_mean"))
-  tmp_sd = setNames(tmp_summary[,"SD"], paste0(c(alpha_names, beta_names, prob_names), "_sd"))
-  return(cbind(spec_id = as.character(spec), as.data.frame(t(c(tmp_means, tmp_sd)))))
+SSOM_estimates = bind_rows(lapply(species_final$spec_id, FUN = function(spec){
+  print(spec)
+  if(!any(grepl(spec, list.files("Data/models_fit/SSOM/run_nov_19/")))){return(NULL)}
+  load(list.files("Data/models_fit/SSOM/run_nov_19/", pattern = as.character(spec), full.names = T))
+  tmp_summary = jags_samples$summary[,c("mean", "sd")] %>% 
+    as_tibble(rownames = "param") %>% 
+    mutate(param = replace(param, list = grepl("alpha_coef", param), values = c("elev", "elev_sq", "day", "day_sq"))) %>% 
+    mutate(param = replace(param, list = grepl("beta_coef", param), values = c("ddeg0","bio_12","rad","asp","slp","ddeg0_sq","bio_12_sq","rad_sq","asp_sq","slp_sq"))) %>% 
+    pivot_wider(names_from = param, values_from = c(mean, sd))
 }))
-
 
 detection_df = right_join(species_final, model_estimates) %>% 
   left_join(traits_final) %>% 
   mutate(mean_p_conv = antilogit(mean_p_mean))
 
-model_1 = glm(mean_p_conv ~ generations + color + saturation + brightness + body_area, family = "binomial", data = detection_df)
+for(t in colnames(traits_final)[-1]){
+  plot(pull(detection_df, "mean_p_conv") ~ pull(detection_df, t))
+}
+
+model_1 = glm(mean_p_conv ~ Vol_min, family = "binomial", data = detection_df)
 summary(model_1)
 
 # TODO add random effects for families
